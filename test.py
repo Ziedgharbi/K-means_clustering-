031b4fd801786918d649c68ec0c18e46651cde05
GHARBI Zied <117041818+Ziedgharbi@users.noreply.github.com>
2023-05-10 12:10:59 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ from sklearn.datasets import make_blobs
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import random
-
 random.seed(1000)
 #### exemple 1 : 2 feature
 
